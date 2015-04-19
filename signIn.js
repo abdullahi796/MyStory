663379e7f4e3b1fb72485cd355958e12a08f9dde
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2015-04-19 13:47:56 +0000

    Updated files

---
+++ signIn.js
@@ -0,0 +1,20 @@
+(function() {
+  var app = angular.module('SignIn', []);
+  app.controller('SignInController',function(){
+    this.username = "";
+    this.password = "";
+    this.signIn = function(){
+        Parse.initialize("j6hmZIae3pafDLIjDGpCntLonB075YYEr7s7dht0", "icc9F3VUzoYqwyZjp8FArXzMQZnO6IiCfDxhD1Iy");
+        Parse.User.logIn(this.username, this.password, {
+          success: function(user) {
+            alert("wooow");
+            window.location.href = "https://game-abdullahi11-1.c9.io/index.html";
+          },
+          error: function(user, error) {
+            alert(error);
+            // The login failed. Check error to see why.
+          }
+        });
+    }
+  });
+})();
