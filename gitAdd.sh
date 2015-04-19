9c9e0e13e69b8902ca3981ac06eabe34e2b78656
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2015-04-19 22:39:14 +0000

    Update git bash script

--- gitAdd.sh
+++ gitAdd.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 echo "Adding to stage"
+echo "  "
+echo "  "
 git add .
 
 echo "Enter Message"
 read NAME
 git commit -m "$NAME"
+echo "  "
+echo "  "
+
 echo "Commiting"
+echo "  "
+echo "  "
 
 git push -u origin master
 
 echo "Success update repo"
+echo "  "
+echo "  "
