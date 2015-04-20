e1131cb3b7517d06d80c728b972561a1b3fec76c
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2015-04-20 02:28:26 +0000

    Updated ConsoleApp

--- bash/commandApp.sh
+++ bash/commandApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while [ "$input" != "exit" ]; do
-    echo "Enter dir to make a directory , file to make a File or exit to quit the program"
+    echo "Enter dir to make a directory , file to make a File , fdir to make a file in a file in a existing dir or exit to quit the program"
     read input
      if [ "$input" = "dir" ]; then
         echo "Enter dir names"
@@ -11,5 +11,11 @@ while [ "$input" != "exit" ]; do
         echo "Enter file dir names"
         read fileName
         echo "$null" >> "$dirName/$fileName"
+    fi
+    
+    if [ "$input" = "fdir" ]; then
+        echo "Enter dir > file names"
+        read fileName
+        echo "$null" >> "$fileName"
     fi
 done
